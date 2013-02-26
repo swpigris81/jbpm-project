fc456a1f9ee84bdff81b52f921a0dc4b7f1dd23d
swpigris81@gmail.com <swpigris81@gmail.com@064b67cc-1500-25b9-0c92-254bfb398b19>
2013-02-26 01:15:32 +0000

    20130226初始版本，已实现局域网内推送消息 下一步将需要优化本版本，需要判断断线时如何重连

---
+++ android/FindMyDroid/src/com/webservice/findmydroid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.webservice.findmydroid;
+
+
+import com.webservice.findmydroid.client.ServiceManager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d("DemoAppActivity", "onCreate()...");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Settings
+        Button okButton = (Button) findViewById(R.id.btn_settings);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                ServiceManager.viewNotificationSettings(MainActivity.this);
+            }
+        });
+
+        // Start the service
+        ServiceManager serviceManager = new ServiceManager(this);
+        serviceManager.setNotificationIcon(R.drawable.notification);
+        serviceManager.startService();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    
+}
