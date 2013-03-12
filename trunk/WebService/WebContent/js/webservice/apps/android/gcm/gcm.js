ae01d278df58e685c2043080255c644785638ce8
swpigris81@gmail.com <swpigris81@gmail.com@064b67cc-1500-25b9-0c92-254bfb398b19>
2013-03-12 08:36:51 +0000

    20130312 完成GCM与服务器端整合

---
+++ trunk/WebService/WebContent/js/webservice/apps/android/gcm/gcm.js
@@ -0,0 +1,259 @@
+/**
+ * JPush推送
+ */
+function jpush(){
+    /**
+     * userReader - 用户信息解析器
+     */
+    var userReader = new Ext.data.JsonReader({
+        totalProperty : "totalCount",
+        root : "userList"
+    },[
+       {name:"id"},//编号
+        {name:"userName"},//用户名
+        {name:"regisId"}//设备号
+    ]);
+    
+    /**
+     * 用户信息数据集
+     */
+    var userStore = new Ext.data.Store({
+        proxy:new Ext.data.HttpProxy({
+            url: path + "/gcm/gcmUserList.action?method=userList"
+        }),
+        reader:userReader,
+        //倒叙排序
+        baseParams:{start:0, limit:50},
+        listeners:{
+            "loadexception":function(loader, node, response){
+                try{
+                    if(response.status == "200"){
+                        try{
+                            var re = Ext.decode(response.responseText);
+                            if(re){
+                                Ext.Msg.alert('错误提示',re.msg, function(btn){
+                                });
+                            }
+                        }catch(e){
+                            Ext.Msg.alert('错误提示',"系统错误！错误代码："+e, function(btn){
+                            });
+                        }
+                    }else{
+                        httpStatusCodeHandler(response.status);
+                    }
+                }catch(e){
+                    Ext.Msg.alert('错误提示',"系统错误！错误代码："+e, function(btn){
+                    });
+                }
+            }
+        }
+    });
+    
+    /**
+     * 数据展现样式
+     */
+    function getSM(){
+        var sm = new Ext.grid.CheckboxSelectionModel();
+        return sm
+    }
+    
+    /**
+     * 用户展现
+     */
+    var userCM = new Ext.grid.ColumnModel([new Ext.grid.RowNumberer(),getSM(),{
+        dataIndex:"id",
+        hidden:true,
+        hideable:false//不允许将隐藏的字段显示出来
+    },{
+        header:"用户名称",
+        dataIndex:"userName",
+        width:150
+    },{
+        header:"设备注册号",
+        dataIndex:"regisId",
+        width:180
+    }]);
+    
+    /**
+     * 用户列表
+     */
+    var userGrid = new Ext.grid.GridPanel({
+        title:"用户列表",
+        region:'center',
+        collapsible:false,//是否可以展开
+        animCollapse:true,//展开时是否有动画效果
+        autoScroll:true,
+        width:Ext.get("gcm_div").getWidth(),
+        height:Ext.get("gcm_div").getHeight() - 20,
+        loadMask:true,//载入遮罩动画（默认）
+        frame:true,
+        autoShow:true,
+        store:userStore,
+        cm:userCM,
+        sm:getSM(),
+        viewConfig:{forceFit:true},//若父容器的layout为fit，那么强制本grid充满该父容器
+        split: true,
+        stripeRows: true,
+        renderTo:"gcm_div",
+        bbar:new Ext.PagingToolbar({
+            pageSize:50,//每页显示数
+            store:userStore,
+            displayInfo:true,
+            displayMsg:"显示{0}-{1}条记录，共{2}条记录",
+            nextText:"下一页",
+            prevText:"上一页",
+            emptyMsg:"无相关记录"
+        }),
+        tbar:[]
+    });
+    
+    /**
+     * 按钮存储器，尚未执行查询
+     */
+    var buttonRightStore = buttonRight();
+    /**
+     * 执行权限按钮加载, 并且加载列表数据, 显示权限按钮
+     * see buttonRight.js
+     */
+    loadButtonRight(buttonRightStore, userStore, userGrid, "gcm_div");
+    /**
+     * 推送
+     */
+    this.push = function(url){
+        var gridSelectionModel = userGrid.getSelectionModel();
+        var gridSelection = gridSelectionModel.getSelections();
+        if(gridSelection.length < 1){
+            Ext.MessageBox.alert('提示','请要推送消息的用户！');
+            return false;
+        }
+        var userArray = new Array();
+        var imeiArray = new Array();
+        for(var i=0; i<gridSelection.length; i++){
+            var userName = gridSelection[i].get("regisId");
+            userArray.push(userName);
+        }
+        var userForm = showPushForm(url, userArray.join(","));
+        var button = [{
+            text:"推送消息",
+            handler:function(){
+                if(userForm.form.isValid()){
+                    push("pushWindow", userForm);
+                }
+            }
+        },{
+            text:"关闭窗口",
+            handler:function(){
+                var userWindow = Ext.getCmp("pushWindow");
+                if(userWindow){
+                    userWindow.close();
+                }
+            }
+        }];
+        showAllWindow("pushWindow", "推送消息",500, 320, userForm, null, button);
+    };
+    /**
+     * 推送表单
+     * @param url
+     * @param users
+     * @param imeis
+     * @returns {Ext.form.FormPanel}
+     */
+    function showPushForm(url, users){
+        var userForm = new Ext.form.FormPanel({
+            frame: true,
+            labelAlign: 'right',
+            labelWidth:60,
+            autoScroll:false,
+            waitMsgTarget:true,
+            url:url,
+            items:[{
+                layout:'column',
+                height:40,
+                items:[{
+                    columnWidth:.90,
+                    layout:'form',
+                    items:[getTextField("regisIdArray", "推送设备", false, true, users)]
+                }]
+            },{
+                layout:'column',
+                height:40,
+                items:[{
+                    columnWidth:.90,
+                    layout:'form',
+                    items:[getTextField("messageTitle", "推送标题", false, false)]
+                }]
+            },{
+                layout:'column',
+                height:70,
+                items:[{
+                    columnWidth:.90,
+                    layout:'form',
+                    items:[getTextAreaField("messageContent", "推送消息", false, false)]
+                }]
+            }]
+        });
+        return userForm;
+    }
+    /**
+     * 推送消息
+     */
+    function push(windowId, form){
+        Ext.MessageBox.show({
+            msg:"正在推送消息，请稍候...",
+            progressText:"正在推送消息，请稍候...",
+            width:300,
+            wait:true,
+            waitConfig: {interval:200},
+            icon:Ext.Msg.INFO
+        });
+        form.getForm().submit({
+            timeout:60000,
+            success: function(form, action) {
+                Ext.Msg.hide();
+                try{
+                    var result = Ext.decode(action.response.responseText);
+                    if(result && result.success){
+                        var msg = "消息推送成功！";
+                        if(result.msg){
+                            msg = result.msg;
+                        }
+                        Ext.Msg.alert('系统提示信息', msg, function(btn, text) {
+                            if (btn == 'ok') {
+                                Ext.getCmp(windowId).close();
+                            }
+                        });
+                    }else if(!result.success){
+                        var msg = "消息推送失败！";
+                        if(result.msg){
+                            msg = result.msg;
+                        }
+                        Ext.Msg.alert('系统提示信息', msg);
+                    }
+                }catch(e){
+                    Ext.Msg.alert('系统提示信息', "系统错误：" + e);
+                }
+            },
+            failure: function(form, action) {//action.result.errorMessage
+                Ext.Msg.hide();
+                var msg = "消息推送失败，请检查您的网络连接或者联系管理员！";
+                try{
+                    var result = Ext.decode(action.response.responseText);
+                    if(result.msg){
+                        msg = result.msg;
+                    }
+                }catch(e){
+                    msg = "系统错误：" + e;
+                }
+                Ext.Msg.alert('系统提示信息', msg);
+            }
+        });
+    }
+}
+/**
+ * 程序主入口
+ */
+Ext.onReady(function(){
+    Ext.QuickTips.init();
+    Ext.form.Field.prototype.msgTarget = 'side';
+    jpush();
+});
---
+++ trunk/WebService/src/android/gcm/com/webservice/gcm/action/GoogleGcmAction.java
@@ -0,0 +1,286 @@
+package com.webservice.gcm.action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.util.CollectionUtils;
+
+import com.google.android.gcm.server.Message;
+import com.google.android.gcm.server.Message.Builder;
+import com.google.android.gcm.server.MulticastResult;
+import com.google.android.gcm.server.Result;
+import com.google.android.gcm.server.Sender;
+import com.webservice.common.action.BaseAction;
+import com.webservice.gcm.bean.GcmModel;
+import com.webservice.gcm.service.GcmService;
+import com.webservice.system.common.constants.Constants;
+import com.webservice.system.role.bean.RoleInfo;
+import com.webservice.system.role.bean.UserRole;
+import com.webservice.system.role.service.IRoleService;
+import com.webservice.system.role.service.IUserRoleService;
+import com.webservice.system.user.bean.UserInfo;
+import com.webservice.system.user.service.IUserService;
+import com.webservice.system.util.CipherUtil;
+
+/** 
+ * <p>Description: [GoogleGCM服务器端]</p>
+ * @author  <a href="mailto: swpigris81@126.com">大牙</a>
+ * @version v1.0
+ */
+public class GoogleGcmAction extends BaseAction {
+    private String regisId;
+    private String regisIdArray;
+    private String userName;
+    private String userPass;
+    private String messageContent;
+    private String messageTitle;
+    private String phoneImei;
+    /**
+     * 用户服务
+     */
+    private IUserService userService;
+    /**
+     * 角色服务
+     */
+    private IRoleService roleService;
+    /**
+     * 用户角色服务
+     */
+    private IUserRoleService userRoleService;
+    /**
+     * 系统默认角色名
+     */
+    private String systemRoleName;
+    /**
+     * GCM服务
+     */
+    private GcmService gcmService;
+    
+    /**
+     * <p>Discription:[Android程序用来发送从GCM收到的注册ID, 服务器就可以对注册ID和用户设备进行关联]</p>
+     * @return
+     * @author:大牙
+     * @update:2013-3-12
+     */
+    public String registration(){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        if(userName == null || "".equals(userName.trim())){
+            resultMap.put("success", false);
+            resultMap.put("msg", "用户名不能为空！");
+        }else if(regisId == null || "".equals(regisId.trim())){
+            resultMap.put("success", false);
+            resultMap.put("msg", "注册设备唯一ID不能为空！");
+        }else{
+            List list = gcmService.findByRegisterId(regisId);
+            if(list != null && !regisId.isEmpty()){
+                resultMap.put("success", false);
+                resultMap.put("msg", "该设备唯一ID已经被注册过！");
+            }else{
+                List userList = userService.getUserByName(userName);
+                if(userList != null && !userList.isEmpty()){
+                    //用户已注册，判断其密码
+                    String checkPass= "from UserInfo where userName = ? and password = ?";
+                    userList = userService.findUserByPageCondition(checkPass, 0, 999999, new Object[]{userName, userPass});
+                    if(userList != null && !userList.isEmpty()){
+                        //密码验证通过，允许每个用户注册多个设备
+                        GcmModel model = new GcmModel();
+                        model.setRegisId(regisId);
+                        model.setUserName(userName);
+                        gcmService.saveGcmRegis(model);
+                        resultMap.put("success", true);
+                        resultMap.put("msg", "用户设备注册成功！");
+                    }else{
+                        resultMap.put("success", false);
+                        resultMap.put("msg", "用户密码错误！");
+                    }
+                }else{
+                    //尚未注册用户，先注册用户
+                    UserInfo user = new UserInfo();
+                    user.setUserName(userName);
+                    user.setPassword(CipherUtil.generatePassword(userPass, userName));
+                    user.setPhoneImei(phoneImei);
+                    this.userService.saveOrUpdate(user);
+                    //为手机注册用户赋予角色权限
+                    if(this.systemRoleName != null && !"".equals(this.systemRoleName)){
+                        List roles = this.roleService.findRoleByName(systemRoleName);
+                        if(roles!=null && roles.size()>0){
+                            RoleInfo role = (RoleInfo) roles.get(0);
+                            String roleId = role.getRoleId();
+                            
+                            UserRole userRole = new UserRole();
+                            userRole.setUserId(user.getUserName());
+                            userRole.setRoleId(roleId);
+                            this.userRoleService.saveOrUpdate(userRole);
+                        }
+                    }
+                    GcmModel model = new GcmModel();
+                    model.setRegisId(regisId);
+                    model.setUserName(userName);
+                    gcmService.saveGcmRegis(model);
+                    resultMap.put("success", true);
+                    resultMap.put("msg", "用户设备注册成功！");
+                }
+            }
+        }
+        writeMeessage(resultMap);
+        return null;
+    }
+    /**
+     * <p>Discription:[服务器注销ID]</p>
+     * @return
+     * @author:大牙
+     * @update:2013-3-12
+     */
+    public String unRegistration(){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        if(userName == null || "".equals(userName.trim())){
+            resultMap.put("success", false);
+            resultMap.put("msg", "用户名不能为空！");
+        }else if(regisId == null || "".equals(regisId.trim())){
+            resultMap.put("success", false);
+            resultMap.put("msg", "注册设备唯一ID不能为空！");
+        }else{
+            List list = gcmService.findByRegisterId(regisId);
+            if(list != null && !list.isEmpty()){
+                gcmService.unRegistion(list);
+                resultMap.put("success", true);
+                resultMap.put("msg", "用户设备注销成功！");
+            }else{
+                resultMap.put("success", false);
+                resultMap.put("msg", "该设备尚未注册！");
+            }
+        }
+        writeMeessage(resultMap);
+        return null;
+    }
+    /**
+     * <p>Discription:[服务器消息推送]</p>
+     * @return
+     * @author:大牙
+     * @update:2013-3-12
+     */
+    public String gcmSender(){
+        Sender sender = new Sender(Constants.GCM_API_KEY);
+        Builder messageBuilder = new Message.Builder();
+        messageBuilder.addData("title", messageTitle);
+        messageBuilder.addData("message", messageContent);
+        Message message = messageBuilder.build();
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        try {
+            if(regisIdArray == null || "".equals(regisIdArray.trim())){
+                resultMap.put("success", false);
+                resultMap.put("msg", "请选择要推送消息的设备");
+            }else{
+                String [] regisIds = regisIdArray.split(",");
+                //多人推送
+                List<String> devices = CollectionUtils.arrayToList(regisIds);
+                //如果GCM服务器无效的话，有5次重发机会
+                MulticastResult multicastResult = sender.send(message, devices, 5);
+                List<Result> results = multicastResult.getResults();
+                resultMap.put("success", true);
+                resultMap.put("msg", "服务器推送结果：");
+                List resultList = new ArrayList();
+                for(int i=0, j=devices.size(); i<j; i++){
+                    String regId = devices.get(i);
+                    Result result = results.get(i);
+                    String messageId = result.getMessageId();
+                    if (messageId != null) {
+                        LOG.info("Succesfully sent message to device: " + regId +
+                                "; messageId = " + messageId);
+                        String canonicalRegId = result.getCanonicalRegistrationId();
+                        if (canonicalRegId != null) {
+                            LOG.info("canonicalRegId " + canonicalRegId);
+                        }
+                        resultList.add(new String[]{regId, "消息推送成功，返回的消息编号：" + messageId});
+                    }else{
+                        String error = result.getErrorCodeName();
+                        if (error.equals(com.google.android.gcm.server.Constants.ERROR_NOT_REGISTERED)) {
+                            //application has been removed from device - unregister it
+                            LOG.info("Unregistered device: " + regId);
+                            resultList.add(new String[]{regId, "消息推送失败，失败原因：用户设备已将该应用删除！"});
+                        }else{
+                            LOG.info("Error sending message to " + regId + ": " + error);
+                            resultList.add(new String[]{regId, "消息推送失败，失败原因：" + error});
+                        }
+                    }
+                }
+                resultMap.put("result", resultList);
+            }
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+            resultMap.put("success", false);
+            resultMap.put("msg", "服务器推送异常！" + e.getMessage());
+        }
+        writeMeessage(resultMap);
+        return null;
+    }
+    /**
+     * <p>Discription:[注册用户列表]</p>
+     * @return
+     * @author:大牙
+     * @update:2013-3-12
+     */
+    public String userList(){
+        List userList = gcmService.findRegisList(start, limit);
+        int size = gcmService.findRegisList(-1, -1).size();
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("success", true);
+        resultMap.put("userList", userList);
+        resultMap.put("totalCount", size);
+        writeMeessage(resultMap);
+        return null;
+    }
+    
+    public String getRegisId() {
+        return regisId;
+    }
+    public void setRegisId(String regisId) {
+        this.regisId = regisId;
+    }
+    public String getRegisIdArray() {
+        return regisIdArray;
+    }
+    public void setRegisIdArray(String regisIdArray) {
+        this.regisIdArray = regisIdArray;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getUserPass() {
+        return userPass;
+    }
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+    public String getMessageContent() {
+        return messageContent;
+    }
+    public void setMessageContent(String messageContent) {
+        this.messageContent = messageContent;
+    }
+    public String getMessageTitle() {
+        return messageTitle;
+    }
+    public void setMessageTitle(String messageTitle) {
+        this.messageTitle = messageTitle;
+    }
+    public IUserService getUserService() {
+        return userService;
+    }
+    public void setUserService(IUserService userService) {
+        this.userService = userService;
+    }
+    public GcmService getGcmService() {
+        return gcmService;
+    }
+    public void setGcmService(GcmService gcmService) {
+        this.gcmService = gcmService;
+    }
+    
+}
